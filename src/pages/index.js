0877484b197b7512f07867f680adb26c4ac1bb96
shamming <wsm1685087768@gmail.com>
2023-10-07 20:54:07 +0800

    add spring

--- src/pages/index.js
+++ src/pages/index.js
@@ -64,6 +64,7 @@ export default function Index() {
           <img alt="C++" src="/img/badge/C++.svg" />
           <img alt="JAVA" src="/img/badge/JAVA.svg" />
           <img alt="Python" src="/img/badge/PYTHON.svg" />
+          <img alt="Spring" src="/img/badge/Spring.svg" />
           <img alt="HTML5" src="/img/badge/HTML5.svg" />
           <img alt="CSS3" src="/img/badge/CSS3.svg" />
           <img alt="JavaScript" src="/img/badge/JAVASCRIPT.svg" />
